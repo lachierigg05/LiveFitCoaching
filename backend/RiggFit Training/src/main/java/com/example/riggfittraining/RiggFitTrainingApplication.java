70df0cb308ec3f0d3b7e40dbb14ffa10b016d58c
lachierigg <lachrigg@gmail.com>
2024-12-05 10:47:46 +1100

    Initial setup of springboot backend

---
+++ backend/RiggFit Training/src/main/java/com/example/riggfittraining/RiggFitTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.riggfittraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiggFitTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiggFitTrainingApplication.class, args);
+    }
+
+}
---
+++ backend/RiggFit Training/src/test/java/com/example/riggfittraining/RiggFitTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riggfittraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiggFitTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
